fc835cc25cf611c19345f3f75f225277448f02a7
Fabian Vilers <fabian.vilers@dev-one.com>
2019-11-19 11:07:04 +0100

    Get tasks from firestore

--- src/containers/Tasks.tsx
+++ src/containers/Tasks.tsx
@@ -1,17 +1,64 @@
-import React from "react";
-import TaskList from "../components/TaskList";
+import React, { useEffect, useReducer } from "react";
 import { TaskModel } from "../models";
+import firebase from "../firebase";
+import TaskList from "../components/TaskList";
+
+type State = {
+  tasks: Array<TaskModel>;
+};
+
+type Action = {
+  payload: TaskModel;
+  type: string;
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "added":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload]
+      };
+    case "modified":
+      return {
+        ...state,
+        tasks: state.tasks.map(task =>
+          task.id !== action.payload.id ? task : action.payload
+        )
+      };
+    case "removed":
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload.id)
+      };
+    default:
+      return state;
+  }
+};
+
+const INITIAL_STATE: State = {
+  tasks: new Array<TaskModel>()
+};
 
 const Tasks: React.FC = () => {
-  const tasks: Array<TaskModel> = [
-    {
-      done: false,
-      id: "1",
-      label: "This is a task"
-    }
-  ];
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-  return <TaskList tasks={tasks} />;
+  useEffect(
+    () =>
+      firebase
+        .firestore()
+        .collection("tasks")
+        .onSnapshot(snapshot => {
+          for (const { doc, type } of snapshot.docChanges()) {
+            const payload = { id: doc.id, ...doc.data() } as TaskModel;
+
+            dispatch({ type, payload });
+          }
+        }),
+    []
+  );
+
+  return <TaskList tasks={state.tasks} />;
 };
 
 export default Tasks;
