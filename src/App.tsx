cf9553ca142ec416cc4438eddfd602b52cc2dadb
Fabian Vilers <fabian.vilers@dev-one.com>
2019-11-19 10:27:23 +0100

    Add the Tasks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Tasks from "./containers/Tasks";
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Tasks</h1>
+      <Tasks />
     </div>
   );
 };
---
+++ src/containers/Tasks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tasks: React.FC = () => {
+  return <div>TODO : display list of tasks</div>;
+};
+
+export default Tasks;
