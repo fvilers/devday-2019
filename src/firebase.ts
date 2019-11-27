22c895009a8803120af616e0b6bf1fc6d620351b
Fabian Vilers <fabian.vilers@dev-one.com>
2019-11-27 08:43:35 +0100

    Remove environment variable for firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,7 @@
-import { config } from "dotenv";
 import * as firebase from "firebase/app";
-import "firebase/auth";
 import "firebase/firestore";
 
-config();
-
-const firebaseConfig = {
-  projectId: process.env.REACT_APP_PROJECT_ID
-};
+const firebaseConfig = { projectId: "tasks-devday-2019" };
 
 firebase.initializeApp(firebaseConfig);
 
